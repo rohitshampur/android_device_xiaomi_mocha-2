e5d1642333fb4deb014977d93f7000bb52c51b71
Nihhaar <nihhaar@gmail.com>
2016-11-14 08:38:52 +0530

    Add recovery files and changes in BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,3 +32,33 @@ TARGET_KERNEL_CONFIG := mocha_user_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/mocha
 TARGET_KERNEL_ARCH := arm
 KERNEL_ARCH := arm
+
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/backlight/pwm-backlight/brightness\"
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/xiaomi/mocha-common/recovery/recovery_keys.c
+TARGET_RECOVERY_FSTAB := device/xiaomi/mocha-common/recovery.fstab
+RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/sdcard"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+
+# Graphics
+USE_OPENGL_RENDERER := true
+
+BOARD_USES_STOCK_POLICY := true
+
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WLAN_DEVICE                := bcmdhd
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_STA          := "/etc/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/etc/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P          := "/etc/firmware/fw_bcmdhd_p2p.bin"
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
