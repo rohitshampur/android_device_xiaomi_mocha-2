baa3a375079798edf8eb5a52f5b60e1fba8643e5
Nihhaar <nihhaar@gmail.com>
2016-12-04 17:24:26 +0530

    Update board's hardware and init files to match it.

--- board/kernel.mk
+++ board/kernel.mk
@@ -2,7 +2,7 @@
 TARGET_KERNEL_CONFIG := mocha_user_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/mocha
 TARGET_KERNEL_ARCH := arm
-BOARD_KERNEL_CMDLINE := 'androidboot.hardware=tn8'
+BOARD_KERNEL_CMDLINE := ''
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --dt device/xiaomi/mocha/dt.img --tags_offset 0x00000100
