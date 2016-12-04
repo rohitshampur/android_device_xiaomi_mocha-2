adc655d87201093488a79f5d65ee0e298a8fa583
Nihhaar <nihhaar@gmail.com>
2016-12-04 19:58:02 +0530

    Trying to fix install-recovery.sh error

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 #Vendor
 $(call inherit-product-if-exists, vendor/xiaomi/mocha/mocha-vendor.mk)
 
+$(call inherit-product, build/target/product/full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := cm_mocha
 PRODUCT_DEVICE := mocha
